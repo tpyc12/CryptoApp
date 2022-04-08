5de78ab5aa25dcf5f2ed8fd36388806920191ab8
tpyc12 <tpyc12@yandex.ru>
2022-04-08 21:04:54 +0300

    Include Worker.

--- app/src/main/java/com/myhome/android/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/myhome/android/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -3,19 +3,19 @@ package com.myhome.android.cryptoapp.data.repository
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.myhome.android.cryptoapp.data.database.AppDatabase
 import com.myhome.android.cryptoapp.data.mapper.CoinMapper
-import com.myhome.android.cryptoapp.data.network.ApiFactory
+import com.myhome.android.cryptoapp.data.workers.RefreshDataWorker
 import com.myhome.android.cryptoapp.domain.CoinInfo
 import com.myhome.android.cryptoapp.domain.CoinRepository
-import kotlinx.coroutines.delay
 
 class CoinRepositoryImpl(
-    application: Application
+    private val application: Application
 ) : CoinRepository {
 
     private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
-    private val apiService = ApiFactory.apiService
     private val mapper = CoinMapper()
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
@@ -32,18 +32,12 @@ class CoinRepositoryImpl(
         }
     }
 
-    override suspend fun loadData() {
-        while (true) {
-            try {
-                val topCoins = apiService.getTopCoinsInfo(limit = 50)
-                val fSyms = mapper.mapNamesListToString(topCoins)
-                val jsonContainer = apiService.getFullPriceList(fsyms = fSyms)
-                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
-                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
-                coinInfoDao.insertPriceList(dbModelList)
-            } catch (e: Exception) {
-            }
-            delay(10000)
-        }
+    override fun loadData() {
+        val workManager = WorkManager.getInstance(application)
+        workManager.enqueueUniqueWork(
+            RefreshDataWorker.NAME,
+            ExistingWorkPolicy.REPLACE,
+            RefreshDataWorker.makeRequest()
+        )
     }
 }
