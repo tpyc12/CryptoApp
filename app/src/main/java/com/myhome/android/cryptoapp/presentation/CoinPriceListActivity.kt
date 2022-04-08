83764d5c604bcaeaa5277ef11efdce338303cff6
tpyc12 <tpyc12@yandex.ru>
2022-04-08 20:46:08 +0300

    Finish land scape.

--- app/src/main/java/com/myhome/android/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/myhome/android/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.myhome.android.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.myhome.android.cryptoapp.R
 import com.myhome.android.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.myhome.android.cryptoapp.domain.CoinInfo
 import com.myhome.android.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -22,11 +23,11 @@ class CoinPriceListActivity : AppCompatActivity() {
         binding.rvCoinPriceList.itemAnimator = null
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinPriceInfoActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
+                if (isOnePane()) {
+                    launchCoinPriceListActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchCoinPriceListFragment(coinPriceInfo.fromSymbol)
+                }
             }
         }
 
@@ -34,6 +35,24 @@ class CoinPriceListActivity : AppCompatActivity() {
         viewModel.coinInfoList.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    private fun isOnePane() = binding.fragmentContainer == null
+
+    private fun launchCoinPriceListActivity(fromSymbol: String) {
+        val intent = CoinPriceInfoActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchCoinPriceListFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, CoinPriceInfoFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
 
