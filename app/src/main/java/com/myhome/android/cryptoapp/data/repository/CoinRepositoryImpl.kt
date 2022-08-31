256e489f558590c28b25b17905b20d847019e347
tpyc12 <tpyc12@yandex.ru>
2022-08-31 22:07:32 +0300

    feat: Create worker manager

--- app/src/main/java/com/myhome/android/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/myhome/android/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -3,20 +3,21 @@ package com.myhome.android.cryptoapp.data.repository
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.myhome.android.cryptoapp.data.database.AppDatabase
 import com.myhome.android.cryptoapp.data.mapper.CoinMapper
 import com.myhome.android.cryptoapp.data.network.ApiFactory
+import com.myhome.android.cryptoapp.data.workers.RefreshDataWorker
 import com.myhome.android.cryptoapp.domain.entity.CoinInfo
 import com.myhome.android.cryptoapp.domain.repository.CoinRepository
 import kotlinx.coroutines.delay
 
 class CoinRepositoryImpl(
-    application: Application
+    private val application: Application
 ) : CoinRepository {
 
     private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
-    private val apiService = ApiFactory.apiService
-
     private val mapper = CoinMapper()
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
@@ -34,17 +35,11 @@ class CoinRepositoryImpl(
     }
 
     override suspend fun loadData() {
-        while (true) {
-            try {
-                val topCoins = apiService.getTopCoinsInfo(limit = 10)
-                val fSyms = mapper.mapNamesListToString(topCoins)
-                val jsonContainer = apiService.getFullPriceList(fsyms = fSyms)
-                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
-                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
-                coinInfoDao.insertPriceList(dbModelList)
-            } catch (e: Exception) {
-            }
-            delay(10000)
-        }
+        val workerManager = WorkManager.getInstance(application)
+        workerManager.enqueueUniqueWork(
+            RefreshDataWorker.NAME,
+            ExistingWorkPolicy.REPLACE,
+            RefreshDataWorker.makeRequest()
+        )
     }
 }
---
+++ app/src/main/java/com/myhome/android/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -0,0 +1,44 @@
+package com.myhome.android.cryptoapp.data.workers
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkerParameters
+import com.myhome.android.cryptoapp.data.database.AppDatabase
+import com.myhome.android.cryptoapp.data.mapper.CoinMapper
+import com.myhome.android.cryptoapp.data.network.ApiFactory
+import kotlinx.coroutines.delay
+
+class RefreshDataWorker(context: Context, workerParameters: WorkerParameters) :
+    CoroutineWorker(context, workerParameters) {
+
+    private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
+    private val apiService = ApiFactory.apiService
+
+    private val mapper = CoinMapper()
+
+    override suspend fun doWork(): Result {
+        while (true) {
+            try {
+                val topCoins = apiService.getTopCoinsInfo(limit = 10)
+                val fSyms = mapper.mapNamesListToString(topCoins)
+                val jsonContainer = apiService.getFullPriceList(fsyms = fSyms)
+                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
+                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+                coinInfoDao.insertPriceList(dbModelList)
+            } catch (e: Exception) {
+            }
+            delay(10000)
+        }
+    }
+
+    companion object {
+
+        const val NAME = "RefreshDataWorker"
+
+        fun makeRequest(): OneTimeWorkRequest {
+            return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
+        }
+    }
+}
